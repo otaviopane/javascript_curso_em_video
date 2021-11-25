0417f7abadc46beff12be13f2c62976b3323c7ca
Otavio F. G. Pane <otavio.pane@gmail.com>
2021-11-25 00:02:58 -0300

    Created a simple condition, if without else

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou corretamente!')
+var vel = 60.5
+console.log(`À velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // COndição simples! (não tem else)
+   console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
